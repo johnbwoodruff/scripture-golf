fcb3fdd4ba4b05bc64ac333f9a1fc8fa5e02b2e0
John Woodruff <johnwoodruff91@gmail.com>
2024-05-20 22:35:36 -0600

    Get ios build working

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ionicframework.scripturegolf455436',
   appName: 'Scripture Golf',
-  webDir: 'dist/scripture-golf/browser',
-  server: {
-    url: 'http://192.168.1.209:4200',
-    cleartext: true
-  }
+  webDir: 'dist/scripture-golf/browser'
 };
 
 export default config;
